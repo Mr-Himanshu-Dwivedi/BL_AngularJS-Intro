01e3df124a119eabb31f18be6c6f372680341dff
Mr-Himanshu-Dwivedi <himanshudwivedi2003@gmail.com>
2025-03-22 20:05:38 +0530

    Display with Name Error

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl="../assets/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  errorMessage: string = "";
 
   ngOnInit(): void{
     this.title = "Hello from bridgeLabz.";
@@ -18,5 +19,16 @@ export class AppComponent {
   onClick($event: any){
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event: any){
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (!nameRegex.test(this.userName)) {
+      this.errorMessage = "Name must start with a capital letter and have at least 3 characters.";
+    } else {
+      this.errorMessage = "";
+      return;
+    }
   }
 }
